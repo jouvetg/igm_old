1681b00c54ea1920e3c062702d856d2c961ad1d2
Guillaume Jouvet <guillaume.jouvet@gmail.com>
2023-03-10 11:31:42 +0100

    update/simplify instruction setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (C) 2021-2022 Guillaume Jouvet <guillaume.jouvet@geo.uzh.ch>
+# Copyright (C) 2021-2022 Guillaume Jouvet <guillaume.jouvet@unil.ch>
 # Published under the GNU GPL (Version 3)
 
 from setuptools import setup
@@ -8,22 +8,10 @@ setup(
     name="igm",
     version="1.0.0",
     author="Guillaume Jouvet",
-    author_email="guillaume.jouvet@geo.uzh.ch",
+    author_email="guillaume.jouvet@unil.ch",
     description="The Instructed Glacier Model",
     url="https://github.com/jouvetg/igm",
     license="gpl-3.0",
-    install_requires=[
-        "protobuf<=4.0.0",
-        "tensorflow",
-        "tensorflow-addons",
-        "scipy",
-        "matplotlib",
-        "xarray",
-        "netCDF4",
-        "IPython",
-        "keras",
-    ],
-    extras_require={"gpu": ["tensorflow==2.4.0", "tensorflow-gpu==2.4.0"]},
     py_modules=["igm"],
     scripts=["igm.py"],
 )
